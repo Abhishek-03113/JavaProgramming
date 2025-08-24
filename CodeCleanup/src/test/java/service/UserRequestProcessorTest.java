b1369a9ea706b8c1530221229292f364059a4319
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-08-24 23:37:51 +0530

    modified test

--- CodeCleanup/src/test/java/service/UserRequestProcessorTest.java
+++ CodeCleanup/src/test/java/service/UserRequestProcessorTest.java
@@ -3,6 +3,7 @@ package service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import techverito.application.InputReader;
+import techverito.constants.BasepackTypes;
 import techverito.constants.Messages;
 import techverito.model.User;
 import techverito.service.UserRequestProcessor;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,18 +27,19 @@ public class UserRequestProcessorTest {
         user = new User();
     }
 
+
     @Test
     public void processSelection1Test() {
         processor = new UserRequestProcessor(mock(InputReader.class));
         processor.processSelection1(user);
-        assertEquals(Messages.notSubscribed, user.getSubcription());
+        assertNull(user.getSubcription());
     }
 
     @Test
     public void processSelection2Test() {
         String balance = user.getBalance();
         InputReader readerMock = mock(InputReader.class);
-        when(readerMock.getInput()).thenReturn("100.0f");
+        when(readerMock.getInput()).thenReturn("100.0");
         processor = new UserRequestProcessor(readerMock);
         processor.processSelection2(user);
         assertNotEquals(balance, user.getBalance());
@@ -57,6 +60,7 @@ public class UserRequestProcessorTest {
         processor = new UserRequestProcessor(readerMock);
         processor.processSelection4(user);
         assertEquals("730.0", user.getBalance());
+        assertEquals(BasepackTypes.GoldPack, user.getSubcription());
     }
 
     @Test
@@ -68,8 +72,9 @@ public class UserRequestProcessorTest {
         processor = new UserRequestProcessor(readerMock);
         processor.processSelection4(user);
 
-        // TODO: test insufficient balance message
+        //TODO: test insufficient balance message
         assertEquals("100.0", user.getBalance());
+        assertNull(user.getSubcription());
     }
 
     @Test
@@ -81,13 +86,14 @@ public class UserRequestProcessorTest {
         processor = new UserRequestProcessor(readerMock);
         processor.processSelection4(user);
         assertEquals("865.0", user.getBalance());
+        assertEquals(BasepackTypes.SilverPack, user.getSubcription());
     }
 
     @Test
     public void processSelection5Test() {
         user.setBalance(1000);
         InputReader readerMock = mock(InputReader.class);
-        when(readerMock.getChannelsInput()).thenReturn(Arrays.asList("Zee", "Sony"));
+        when(readerMock.getChannelsInput()).thenReturn(Arrays.asList("ZEE", "SONY"));
         String balance = user.getBalance();
 
         processor = new UserRequestProcessor(readerMock);
@@ -100,7 +106,7 @@ public class UserRequestProcessorTest {
         String email = user.getEmail();
         String phone = user.getPhone();
         InputReader readerMock = mock(InputReader.class);
-        when(readerMock.getInput()).thenReturn("test@gmail.com", "1234567890");
+        when(readerMock.getInput()).thenReturn("test@gmail.com").thenReturn("1234567890");
 
         processor = new UserRequestProcessor(readerMock);
         processor.processSelection6(user);
